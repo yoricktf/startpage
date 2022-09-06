c43676aa7f142f138947b5c792fea665a858a247
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-06 16:37:50 +0200

    added images to src folder and imported into app.js, created array of imported images

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
 import logo from './logo.svg';
+import bbc from './bbc.svg';
+import f1 from './f1.svg';
+import github from './github.svg';
+import gmail from './gmail.svg';
+import googlecalendar from './googlecalendar.svg';
+import linkedin from './linkedin.svg';
+import netflix from './netflix.svg';
+import newyorktimes from './newyorktimes.svg';
+import premierleague from './premierleague.svg';
+import reddit from './reddit.svg';
+import youtube from './youtube.svg';
 import './App.css';
 
+
+
+
+
 function App() {
+
+  const images = [bbc, f1, github, gmail, googlecalendar, linkedin, netflix, newyorktimes, premierleague, reddit, youtube];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
