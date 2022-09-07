1e8a7be9578f408f525b7eb2950e25ce07b31777
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-07 09:23:34 +0200

    importing component into the app.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,39 @@
-import logo from './logo.svg';
+import './App.css';
+import Bookmarks from './Components/Bookmarks/Bookmarks';
+// importing images below
 import bbc from './bbc.svg';
 import f1 from './f1.svg';
 import github from './github.svg';
 import gmail from './gmail.svg';
-import googlecalendar from './googlecalendar.svg';
+import googleCalendar from './googlecalendar.svg';
 import linkedin from './linkedin.svg';
 import netflix from './netflix.svg';
-import newyorktimes from './newyorktimes.svg';
-import premierleague from './premierleague.svg';
+import newYorkTimes from './newyorktimes.svg';
+import premierLeague from './premierleague.svg';
 import reddit from './reddit.svg';
 import youtube from './youtube.svg';
-import './App.css';
-
-
-
-
+import premiumize from './premiumize.svg';
 
 function App() {
+  const websiteObjects = [
+    { name: 'bbc', id: 1, image: bbc, url: 'https://www.bbc.com', },
+    { name: 'f1', id: 2, image: f1, url: 'https://www.formula1.com', },
+    { name: 'github', id: 3, image: github, url: 'https://www.github.com', },
+    { name: 'gmail', id: 4, image: gmail, url: 'https://www.gmail.com', },
+    { name: 'googlecalendar', id: 5, image: googleCalendar, url: 'https://www.googlecalendar.com', },
+    { name: 'linkedin', id: 6, image: linkedin, url: 'https://www.linkedin.com', },
+    { name: 'netflix', id: 7, image: netflix, url: 'https://www.netflix.com', },
+    { name: 'nyTimes', id: 8, image: newYorkTimes, url: 'https://www.nytimes.com/', },
+    { name: 'premierLeague', id: 9, image: premierLeague, url: 'https://www.premierleague.com', },
+    { name: 'premiumize', id: 12, image: premiumize, url: 'https://www.premiumize.me', },
+    { name: 'reddit', id: 10, image: reddit, url: 'https://www.reddit.com', },
+    { name: 'youtube', id: 11, image: youtube, url: 'https://www.youtube.com', },
+  ];
 
-  const websiteObjects = [{ image: bbc, url: 'https://www.bbc.com', color: '' }, { image: f1, url: 'https://www.formula1.com', color: '' }, { image: github, url: 'https://www.github.com', color: '' }, { image: gmail, url: 'https://www.gmail.com', color: '' }, { image: googlecalendar, url: 'https://www.googlecalendar.com', color: '' }, { image: linkedin, url: 'https://www.linkedin.com', color: '' }, { image: netflix, url: 'https://www.netflix.com', color: '' }, { image: newyorktimes, url: 'https://www.nytimes.com/', color: '' }, { image: premierleague, url: 'https://www.premierleague.com', color: '' }, { image: reddit, url: 'https://www.reddit.com', color: '' }, { image: youtube, url: 'https://www.youtube.com', color: '' }];
 
-
-  const goToSite = (e) => {
-    console.log(e);
-    window.open(e, '_blank');
-
-  }
   return (
     <div className="App">
-      {websiteObjects.map((websiteObject) => (
-        <>
-          <img onClick={e => goToSite(e.target.id)} src={websiteObject.image} alt='' id={websiteObject.url} className='logo' />
-        </>
-      ))}
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Bookmarks data={[...websiteObjects]} />
     </div>
   );
 }
