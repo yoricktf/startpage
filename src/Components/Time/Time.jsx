80162214464d0a5caf7e1b67d872a4156ded3b2b
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-07 15:10:09 +0200

    displaying current time with seconds

--- src/Components/Time/Time.jsx
+++ src/Components/Time/Time.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react'
 
 const Time = () => {
+  const [time, setTime] = useState(new Date())
+
+  function refreshClock() {
+    setTime(new Date());
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(refreshClock, 1000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
+  }, []);
+
   return (
-    <div>Time</div>
+    <>
+      <h1>{time.toLocaleTimeString('de-DE')}</h1>
+    </>
   )
 }
 
