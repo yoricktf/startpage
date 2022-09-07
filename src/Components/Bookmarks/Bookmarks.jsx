886cbb3b74c908d2c40b067744590438a0ec66ee
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-07 11:07:55 +0200

    showing details from each website post and linking to their sites

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -3,40 +3,23 @@ import React from 'react'
 
 
 const Bookmarks = (props) => {
-
-
   const websiteObjects = props.data;
-
-  console.log(websiteObjects)
-
-
-
 
   const goToSite = (e) => {
     console.log(e);
     window.open(e, '_blank');
   }
 
-  const hoverEffect = (e) => {
-    console.log(e.title);
-
-  }
-
-
   return (
     <div>
-      <h1>hello world</h1>
       {websiteObjects.map((websiteObject) => (
         <img
           key={websiteObject.id}
           src={websiteObject.image}
-          alt=''
-          onMouseOver={e => hoverEffect(e.target)}
+          alt={`${websiteObject.name}'s logo and link`}
           onClick={e => goToSite(e.target.id)}
-          id={websiteObject.name}
-          title={websiteObject.color}
+          id={websiteObject.url}
           className={`logo ${websiteObject.name}`} />
-
       ))}
     </div>
   )
