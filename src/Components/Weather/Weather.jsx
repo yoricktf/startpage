723888741dddfa40ee653182bd253bde792938de
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-09 09:41:06 +0200

    showing icon denoting the type of weather

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -4,6 +4,7 @@ const Weather = () => {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
   const [data, setData] = useState({});
+  const [icon, setIcon] = useState()
   const [temp, setTemp] = useState()
   const [maxTemp, setMaxTemp] = useState()
   const [minTemp, setMinTemp] = useState()
@@ -22,6 +23,7 @@ const Weather = () => {
           setTemp(result.main.temp)
           setMaxTemp(result.main.temp_max)
           setMinTemp(result.main.temp_min)
+          setIcon(result.weather[0].icon)
         });
     }
     fetchData();
@@ -29,6 +31,7 @@ const Weather = () => {
 
   return (
     <div>
+      <img src={`${process.env.REACT_APP_ICON_URL}${icon}@2x.png`} alt="" />
       <h5>{maxTemp}℃</h5>
       <h3>{temp}℃</h3>
       <h5>{minTemp}℃</h5>
