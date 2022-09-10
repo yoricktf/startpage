da309146d7a2cb967cd2c9872288f7eb3789cbae
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-10 12:53:56 +0200

    component rearranging

--- src/App.js
+++ src/App.js
@@ -4,40 +4,8 @@ import Bookmarks from './Components/Bookmarks/Bookmarks';
 import Time from './Components/Time/Time';
 import Weather from './Components/Weather/Weather';
 import News from './Components/News/News';
-// importing images below
-import bbc from './bbc.svg';
-import f1 from './f1.svg';
-import github from './github.svg';
-import gmail from './gmail.svg';
-import googleCalendar from './googlecalendar.svg';
-import linkedin from './linkedin.svg';
-import netflix from './netflix.svg';
-import newYorkTimes from './newyorktimes.svg';
-import premierLeague from './premierleague.svg';
-import reddit from './reddit.svg';
-import youtube from './youtube.svg';
-import premiumize from './premiumize.svg';
-
 
 function App() {
-  const websiteObjects = [
-    { name: 'bbc', id: 1, image: bbc, url: 'https://www.bbc.com', },
-    { name: 'f1', id: 2, image: f1, url: 'https://www.formula1.com', },
-    { name: 'github', id: 3, image: github, url: 'https://www.github.com', },
-    { name: 'gmail', id: 4, image: gmail, url: 'https://www.gmail.com', },
-    { name: 'googlecalendar', id: 5, image: googleCalendar, url: 'https://www.googlecalendar.com', },
-    { name: 'linkedin', id: 6, image: linkedin, url: 'https://www.linkedin.com', },
-    { name: 'netflix', id: 7, image: netflix, url: 'https://www.netflix.com', },
-    { name: 'nyTimes', id: 8, image: newYorkTimes, url: 'https://www.nytimes.com/', },
-    { name: 'premierLeague', id: 9, image: premierLeague, url: 'https://www.premierleague.com', },
-    { name: 'premiumize', id: 12, image: premiumize, url: 'https://www.premiumize.me', },
-    { name: 'reddit', id: 10, image: reddit, url: 'https://www.reddit.com', },
-    { name: 'youtube', id: 11, image: youtube, url: 'https://www.youtube.com', },
-  ];
-
-
-
-
 
 
   return (
@@ -45,7 +13,7 @@ function App() {
       <Time />
       {/* weather is imported through the news widget */}
       <News />
-      <Bookmarks data={[...websiteObjects]} />
+      <Bookmarks />
     </div>
   );
 }
