ee5e807d8b3ccee204476a3ef8b062c777cb5fb8
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-09-10 12:58:50 +0200

    moved the data into the bookmarks component and added a hover effect for each item

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -1,25 +1,59 @@
 import React from 'react'
 import './Bookmarks.css'
+// importing images below
+import bbc from '../../bbc.svg';
+import f1 from '../../f1.svg';
+import github from '../../github.svg';
+import gmail from '../../gmail.svg';
+import googleCalendar from '../../googlecalendar.svg';
+import linkedin from '../../linkedin.svg';
+import netflix from '../../netflix.svg';
+import newYorkTimes from '../../newyorktimes.svg';
+import premierLeague from '../../premierleague.svg';
+import reddit from '../../reddit.svg';
+import youtube from '../../youtube.svg';
+import premiumize from '../../premiumize.svg';
 
 const Bookmarks = (props) => {
-  const websiteObjects = props.data;
+  const websiteObjects = [
+    { name: 'bbc', id: 1, image: bbc, url: 'https://www.bbc.com', },
+    { name: 'f1', id: 2, image: f1, url: 'https://www.formula1.com', },
+    { name: 'github', id: 3, image: github, url: 'https://www.github.com', },
+    { name: 'gmail', id: 4, image: gmail, url: 'https://www.gmail.com', },
+    { name: 'googlecalendar', id: 5, image: googleCalendar, url: 'https://www.googlecalendar.com', },
+    { name: 'linkedin', id: 6, image: linkedin, url: 'https://www.linkedin.com', },
+    { name: 'netflix', id: 7, image: netflix, url: 'https://www.netflix.com', },
+    { name: 'nyTimes', id: 8, image: newYorkTimes, url: 'https://www.nytimes.com/', },
+    { name: 'premierLeague', id: 9, image: premierLeague, url: 'https://www.premierleague.com', },
+    { name: 'premiumize', id: 12, image: premiumize, url: 'https://www.premiumize.me', },
+    { name: 'reddit', id: 10, image: reddit, url: 'https://www.reddit.com', },
+    { name: 'youtube', id: 11, image: youtube, url: 'https://www.youtube.com', },
+  ];
+
+  // const websiteObjects = props.data;
 
   const goToSite = (e) => {
     console.log(e);
-    window.open(e, '_blank');
+    window.open(e);
   }
 
   return (
-    <div>
+    <div className='bookmarksPalet'>
       {websiteObjects.map((websiteObject) => (
-        <img
-          key={websiteObject.id}
-          src={websiteObject.image}
-          alt={`${websiteObject.name}'s logo and link`}
-          onClick={e => goToSite(e.target.id)}
-          id={websiteObject.url}
-          className={`logo ${websiteObject.name}`} />
+
+        <svg>
+          <image
+            href={websiteObject.image}
+            key={websiteObject.id}
+            alt={`${websiteObject.name}'s logo and link`}
+            onClick={e => goToSite(e.target.id)}
+            id={websiteObject.url}
+            className={`logo ${websiteObject.name}`}
+            style={{ fill: "lightblue", width: "100px", height: "100px" }}
+          />
+        </svg>
       ))}
+
     </div>
   )
 }
