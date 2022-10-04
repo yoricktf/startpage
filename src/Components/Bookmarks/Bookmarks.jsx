5828514d2cb5fd77a3e3a4d8e60f2670adf896e8
Yorick ten Feld <yorick.tenfeld@gmail.com>
2022-10-04 16:46:58 +0200

    set new links, made it open in current tab. fixed key to tag to index

--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -16,32 +16,31 @@ import premiumize from '../../premiumize.svg';
 
 const Bookmarks = (props) => {
   const websiteObjects = [
-    { name: 'bbc', id: 1, image: bbc, url: 'https://www.bbc.com', },
-    { name: 'f1', id: 2, image: f1, url: 'https://www.formula1.com', },
-    { name: 'github', id: 3, image: github, url: 'https://www.github.com', },
-    { name: 'gmail', id: 4, image: gmail, url: 'https://www.gmail.com', },
-    { name: 'googlecalendar', id: 5, image: googleCalendar, url: 'https://www.googlecalendar.com', },
-    { name: 'linkedin', id: 6, image: linkedin, url: 'https://www.linkedin.com', },
-    { name: 'netflix', id: 7, image: netflix, url: 'https://www.netflix.com', },
-    { name: 'nyTimes', id: 8, image: newYorkTimes, url: 'https://www.nytimes.com/', },
-    { name: 'premierLeague', id: 9, image: premierLeague, url: 'https://www.premierleague.com', },
-    { name: 'premiumize', id: 12, image: premiumize, url: 'https://www.premiumize.me', },
-    { name: 'reddit', id: 10, image: reddit, url: 'https://www.reddit.com', },
-    { name: 'youtube', id: 11, image: youtube, url: 'chrome://bookmarks/', },
+    { name: 'bbc', image: bbc, url: 'https://www.bbc.com', },
+    { name: 'f1', image: f1, url: 'http://kf.freestreams-live1.com/', },
+    { name: 'github', image: github, url: 'https://www.github.com', },
+    { name: 'gmail', image: gmail, url: 'https://www.gmail.com', },
+    { name: 'googlecalendar', image: googleCalendar, url: 'https://www.googlecalendar.com', },
+    { name: 'linkedin', image: linkedin, url: 'https://www.linkedin.com', },
+    { name: 'netflix', image: netflix, url: 'https://www.netflix.com', },
+    { name: 'nyTimes', image: newYorkTimes, url: 'https://www.nytimes.com/', },
+    { name: 'premierLeague', image: premierLeague, url: 'https://live7.footybite.cc/', },
+    { name: 'premiumize', image: premiumize, url: 'https://www.premiumize.me', },
+    { name: 'reddit', image: reddit, url: 'https://www.reddit.com', },
+    { name: 'youtube', image: youtube, url: 'https://www.youtube.com/', },
   ];
 
   // const websiteObjects = props.data;
 
   const goToSite = (e) => {
-    console.log(e);
-    // window.open(e);
+    window.open(e, "_self");
   }
 
   return (
     <div className='bookmarksPalet'>
-      {websiteObjects.map((websiteObject) => (
-        <div key={websiteObject.id} >
-          <svg onClick={e => goToSite(e.target.id)} id={websiteObject.url} className={`logo ${websiteObject.name}`}>
+      {websiteObjects.map((websiteObject, index) => (
+        <div key={index} >
+          <svg onClick={e => goToSite(websiteObject.url)} className={`logo ${websiteObject.name}`}>
             <image
               href={websiteObject.image}
               alt={`${websiteObject.name}'s logo and link`}
